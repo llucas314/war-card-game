7c5c6a7f4e39c2d20512974176ce358b0e631f2e
Ren Lucas <llucas314@gmail.com>
2019-10-10 20:16:35 -0400

    added arrays to hold card values

---
+++ main.js
@@ -0,0 +1,8 @@
+let cards = [];
+const ranks = ['1','2','3','4','5','6','7','8','9','10','Jack','Queen','King','Ace'];
+let values = [];
+const suits = ['Spades','Clubs','Diamonds','Hearts'];
+
+for (let i = 0; i < 15; i++){
+    values[i] = i;
+}
