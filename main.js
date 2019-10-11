092c305201c86081f906d22bef62420163556be5
Ren Lucas <llucas314@gmail.com>
2019-10-10 23:20:54 -0400

    added flipTopCard & war functions

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ let cards = [];
 const ranks = ['2','3','4','5','6','7','8','9','10','Jack','Queen','King','Ace'];
 let values = [];
 const suits = ['Spades','Clubs','Diamonds','Hearts'];
-
+let flipped1 = null;
+let flipped2 = null;
 // initialize values array
 for (let i = 0; i < 13; i++){
     values[i] = i + 2;
@@ -33,7 +34,7 @@ for (let i = 0; i < 2; i++){
     players.push({
         player: (i+1),
         cardsInPlay: [],
-        playCard: function(){
+        placeCard: function(){
            this.cardsInPlay.push(this.cardsInHand.pop());
         }
     })
@@ -45,10 +46,37 @@ function dealCards(){
     players[0].cardsInHand = cards.slice(0, cards.length/2);
     players[1].cardsInHand = cards.slice(cards.length/2, cards.length);
 }
-function startRound(){
-    players[0].pla
+function playCards(){
+    players[0].placeCard();
+    players[1].placeCard();
+    flipTopCards();
+    compareCards(flipped1,flipped2);
+}
+function compareCards(card1, card2){
+    if (card1.value === card2.value){
+        war();
+    } else if (card1.value > card2.value){
+        players[0].cardsInHand = [...players[0].cardsInPlay,...players[0].cardsInHand]
+        players[0].cardsInHand = [...players[1].cardsInPlay,...players[0].cardsInHand]
+    } else {
+        players[1].cardsInHand = [...players[1].cardsInPlay,...players[1].cardsInHand]
+        players[1].cardsInHand = [...players[0].cardsInPlay,...players[1].cardsInHand]
+    }
+}
+function flipTopCards(){
+    flipped1 = players[0].cardsInPlay[cardsInPlay.length-1];
+    flipped1.flipCard();
+    flipped2 = players[1].cardsInPlay[cardsInPlay.length-1];
+    flipped2.flipCard();
+
+}
+function war(){
+    playCards();
+    playCards();
+    playCards();
+    flipTopCards();
+    compareCards(flipped1,flipped2);
 }
 shuffleCards();
 dealCards();
-players[0].playCards();
 console.log(players[0])
