b2f17c652b3b4350293659333e127b3f4128c47c
Ren Lucas <llucas314@gmail.com>
2019-10-12 09:22:19 -0400

    created Cards class

--- main2.js
+++ main2.js
@@ -7,3 +7,14 @@ class Deck{
         console.log('*****Shuffling cards*****')
     }
 }
+class Cards{
+    suits = ['Spades','Clubs','Diamonds','Hearts'];
+    ranks = ['2','3','4','5','6','7','8','9','10','Jack','Queen','King','Ace'];
+    values = [2,3,4,5,6,7,8,9,10,11,12,13,14];
+
+    constructor(suit, rank, value){
+        this.suit = suit;
+        this.rank = rank;
+        this.value = value;
+    }
+}
