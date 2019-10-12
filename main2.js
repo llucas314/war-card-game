6b8c0e8d7c17ffc25870af1e0f1ba1686d3c8471
Ren Lucas <llucas314@gmail.com>
2019-10-12 09:37:31 -0400

    created shuffle and init methods for Deck class

--- main2.js
+++ main2.js
@@ -1,17 +1,25 @@
 class Deck{
+    suits = ['Spades','Clubs','Diamonds','Hearts'];
+    ranks = ['2','3','4','5','6','7','8','9','10','Jack','Queen','King','Ace'];
+    values = [2,3,4,5,6,7,8,9,10,11,12,13,14];
+    card;
     constructor(){
         this.cards = [];
+    }
+    init(){
+        for (let i = 0; i < this.suits.length; i++){
+            for(let j = 0; j < this.ranks.length; j++){
+                this.card = new Card(this.suits[i], this.ranks[j], this.values[j]);
+                this.cards.push(this.card);
+            }
+        }
     }
     shuffle(){
         this.cards.sort((a, b) => 0.5 - Math.random())
         console.log('*****Shuffling cards*****')
     }
 }
-class Cards{
-    suits = ['Spades','Clubs','Diamonds','Hearts'];
-    ranks = ['2','3','4','5','6','7','8','9','10','Jack','Queen','King','Ace'];
-    values = [2,3,4,5,6,7,8,9,10,11,12,13,14];
-
+class Card{
     constructor(suit, rank, value){
         this.suit = suit;
         this.rank = rank;
