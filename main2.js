68f0a5d47b6ec476722b12b189e1909aa90a3375
Ren Lucas <llucas314@gmail.com>
2019-10-13 09:58:30 -0400

    updated console outputs

--- main2.js
+++ main2.js
@@ -63,14 +63,14 @@ class Board{
         }
         this.dealCards(deck);
         let round = 1;
-        while(this.checkForWinner()){
+        while(this.gameStillInProgress()){
             this.startRound(round);
             console.log(`***** At the end of round ${round}, ${this.players[0].name} has ${this.players[0].cardsInHand.length} cards and ${this.players[1].name} has ${this.players[1].cardsInHand.length} cards *****`)
             round++;
         }
-        if (this.players[0].cardsInHand.length === 0){
+        if (this.players[1].hasCardsLeft()){
             console.log(`***** Game over. ${this.players[0].name} is out of cards. ${this.players[1].name} won! *****`);
-        } else if (this.players[1].length === 0){
+        } else if (this.players[0].hasCardsLeft()){
             console.log(`***** Game over. ${this.players[1].name} is out of cards. ${this.players[0].name} won! *****`);
         }
     }
@@ -82,29 +82,22 @@ class Board{
             return true;
         } return false;
     }
-    compareCards(playerOneCard, playerTwoCard){
-        if (this.cardsInPlay.length >= 2){
-            playerOneCard = this.cardsInPlay[this.cardsInPlay.length-2].value;
-            playerTwoCard = this.cardsInPlay[this.cardsInPlay.length-1].value;
+    compareCards(){
+            let playerOneCard = this.cardsInPlay[this.cardsInPlay.length-2].value;
+            let playerTwoCard = this.cardsInPlay[this.cardsInPlay.length-1].value;
             if (playerOneCard === playerTwoCard){
                 this.flipTopCards();
                 console.log("It's a tie!")
                 this.war();
             } else if (playerOneCard > playerTwoCard){
                 this.flipTopCards();
-                this.cardsInPlay.sort((a, b) => 0.5 - Math.random())
-                this.players[0].cardsInHand = [...this.cardsInPlay,...this.players[0].cardsInHand]
-                this.cardsInPlay = [];
-                console.log(`***** ${this.players[0].name} wins the round! *****`)
+                this.takeCardsInPlay(this.players[0])
             } else if(playerOneCard < playerTwoCard){
                 this.flipTopCards();
-                this.cardsInPlay.sort((a, b) => 0.5 - Math.random())
-                this.players[1].cardsInHand = [...this.cardsInPlay,...this.players[1].cardsInHand]
-                this.cardsInPlay = []; 
-                console.log(`***** ${this.players[1].name} wins the round! *****`)
+                this.takeCardsInPlay(this.players[1])
             }
-         } else return
-    }
+         }
+    
     flipTopCards(){
         let cip = this.cardsInPlay;
         cip[cip.length-1].isFlipped = true;
@@ -113,13 +106,21 @@ class Board{
         console.log(`${this.players[1].name} flipped up a ${cip[cip.length-1].rank} of ${cip[cip.length-1].suit}`)
     }
     playCards(){
-        if (this.checkForWinner()){
+        if (this.players[0].hasCardsLeft() && this.players[1].hasCardsLeft()){
             let playerOneCard = this.players[0].placeCard();
             let playerTwoCard = this.players[1].placeCard();
             this.cardsInPlay.push(playerOneCard);
             this.cardsInPlay.push(playerTwoCard);
             return true;
-        } else return false;
+        } else if (!this.players[0].hasCardsLeft()){
+            console.log(`***** ${this.players[0].name} does not have enough cards to finish war *****`);
+            this.takeCardsInPlay(this.players[1].name);
+            return false;
+        } else{
+            console.log(`***** ${this.players[1]} does not have enough cards to finish war *****`);
+            this.takeCardsInPlay(this.players[0]);
+            return false;
+        };
     }
     war(){
         if (this.playCards()){
@@ -142,12 +143,18 @@ class Board{
             }
         } else return;
     }
-    checkForWinner(){
-        if ((this.players[0].cardsInHand.length === 0 || this.players[1].cardsInHand.length === 0) && this.cardsInPlay.length === 0){
+    takeCardsInPlay(handWinner){
+        this.cardsInPlay.sort((a, b) => 0.5 - Math.random());
+        handWinner.cardsInHand = [...this.cardsInPlay,...handWinner.cardsInHand];
+        this.cardsInPlay = [];
+        console.log(`***** ${handWinner.name} wins the round! *****`);
+    }
+    gameStillInProgress(){
+        if ((!this.players[0].hasCardsLeft() || !this.players[1].hasCardsLeft()) && this.cardsInPlay.length === 0){
             return false;
         } else return true;
     }
 }
 let board = new Board;
-// board.init();
+board.init();
 // if player x does not have a card to playl player y needs to pick them up. something in war.
